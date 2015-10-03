2609331bf464b6ab9ceefbc13f3dfcb668cd6b57
Dan Cox <danzabian@gmail.com>
2015-10-03 13:27:56 +0000

    Setting up the project base

---
+++ src/Warden.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Warden;
+
+/**
+ * The base class for warden, to start/stop and control various actions
+ *
+ * @package Warden
+ * @author Dan Cox
+ */
+class Warden
+{
+
+} // END class Warden
